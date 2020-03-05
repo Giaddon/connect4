06bcc1e35b794f34ac9938695eead6d27c2aa6ce
Graham Trail <Graham@desmond.localdomain>
2020-03-05 12:07:55 -0800

    Game board code

--- connect4.js
+++ connect4.js
@@ -5,18 +5,26 @@
  * board fills (tie)
  */
 
-var WIDTH = 7;
-var HEIGHT = 6;
+const WIDTH = 7;
+const HEIGHT = 6;
 
-var currPlayer = 1; // active player: 1 or 2
-var board = []; // array of rows, each row is array of cells  (board[y][x])
+let currPlayer = 1; // active player: 1 or 2
+const BOARD = []; // array of rows, each row is array of cells  (board[y][x])
 
 /** makeBoard: create in-JS board structure:
  *    board = array of rows, each row is array of cells  (board[y][x])
  */
 
 function makeBoard() {
-  // TODO: set "board" to empty HEIGHT x WIDTH matrix array
+  // TODO: set "board" to empty HEIGHT x WIDTH matrix array, value of 0 for each cell
+  for (let i = 0; i < HEIGHT; i++) {
+    let row = [];
+    for (let j = 0; j < WIDTH; j++) {
+      row.push(0);
+    }
+    BOARD.push(row)
+  }
+  console.log(BOARD);
 }
 
 /** makeHtmlBoard: make HTML table and row of column tops. */
@@ -130,4 +138,4 @@ function checkForWin() {
 }
 
 makeBoard();
-makeHtmlBoard();
+//makeHtmlBoard();
